ebdb9dc0fad36e538d9594d053758b3429909407
Jagesh07 <148576127+Jagesh07@users.noreply.github.com>
2024-02-27 07:15:02 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accounId=12345
+let accountEmail="vwalke09@gmail.com"
+var accountPass="1234"
+acccounCity="gondia"
+let  accountState;
+//changing details
+
+accountEmail="jageshwalke99@gmail"
+accountPass="45668"
+acccounCity="nagpur"
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+console.log(accounId)
+console.table([acccounCity,accountEmail,accountPass,accountState])
