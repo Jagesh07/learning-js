8f8b1abe216fc78d812c331e77c54a9707ef2ad4
Jagesh07 <148576127+Jagesh07@users.noreply.github.com>
2024-02-28 17:52:10 +0000

    convertion of datatype

---
+++ 01_basic/02_conversion.js
@@ -0,0 +1,25 @@
+let score=33
+console.log(typeof score )//first method
+console.log((typeof score)) //seond method
+
+ let valueInNumber=Number(score)
+ console.log(typeof valueInNumber);
+ // convert 
+ let valeInNUMBER= Number(score)
+ console.log(typeof valeInNUMBER)
+ console.log(valeInNUMBER)
+ //"33"=>33
+ // "33abc"=>NaN
+ //TRUE=>1; FALSE=>0
+ let loggedIn=1
+ let booleanIsLoggedIn=Boolean(loggedIn)
+ console.log(booleanIsLoggedIn) //convert
+
+
+
+
+ let somenumber=16
+ let stringnumber=String(somenumber)
+ console.log(stringnumber) // convertion
+ console.log(typeof stringnumber) //convertion
+
